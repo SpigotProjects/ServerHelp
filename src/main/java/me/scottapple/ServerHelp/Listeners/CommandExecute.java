f791747f29c86cf9034f936e8338772f9fb003e1
Matthew Browning <MC.Fallen>
2014-10-14 14:47:06 -0700

    Commit

--- src/main/java/me/scottapple/ServerHelp/Listeners/CommandExecute.java
+++ src/main/java/me/scottapple/ServerHelp/Listeners/CommandExecute.java
@@ -7,7 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-//
+
 public class CommandExecute implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
